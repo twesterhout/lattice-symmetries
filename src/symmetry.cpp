091b46600f3118befcbe7d79465a12f7f214780d
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-08-28 01:23:39 +0200

    Implement symmetry ADT

---
+++ src/symmetry.cpp
@@ -0,0 +1,203 @@
+#include "symmetry.hpp"
+#include <algorithm>
+#include <memory>
+#include <numeric>
+
+namespace lattice_symmetries {
+
+// \p permutation must be a valid permutation!
+template <class Int> auto compute_periodicity(tcb::span<Int const> permutation) -> unsigned
+{
+    auto const is_id = [](auto const& xs) {
+        auto y = Int{0};
+        for (auto const x : xs) {
+            if (x != y) { return false; }
+            ++y;
+        }
+        return true;
+    };
+    std::vector<unsigned> a(permutation.size());
+    std::vector<unsigned> b(permutation.size());
+    std::iota(std::begin(a), std::end(a), Int{0});
+    auto periodicity = 0U;
+    for (;;) {
+        ++periodicity;
+        std::transform(std::begin(permutation), std::end(permutation), std::begin(b),
+                       [&a](auto const i) { return a[i]; });
+        if (is_id(b)) { break; }
+        std::swap(a, b);
+    }
+    return periodicity;
+}
+
+template auto compute_periodicity(tcb::span<uint16_t const> permutation) -> unsigned;
+template auto compute_periodicity(tcb::span<unsigned const> permutation) -> unsigned;
+
+auto compute_eigenvalue(unsigned sector, unsigned periodicity) noexcept -> std::complex<double>
+{
+    if (sector == 0U) { return std::complex<double>{1.0, 0.0}; }
+    if (2U * sector == periodicity) { return std::complex<double>{-1.0, 0.0}; }
+    auto const arg = -static_cast<long double>(2U * sector) / static_cast<long double>(periodicity);
+    return std::complex<double>{static_cast<double>(std::cos(M_PIl * arg)),
+                                static_cast<double>(std::sin(M_PIl * arg))};
+}
+
+auto get_state_info(tcb::span<batched_small_symmetry_t const> const batched_symmetries,
+                    tcb::span<small_symmetry_t const> const symmetries, uint64_t bits,
+                    uint64_t& representative, std::complex<double>& character,
+                    double& norm) noexcept -> void
+{
+    constexpr auto       batch_size = batched_small_symmetry_t::batch_size;
+    alignas(32) uint64_t initial[batch_size];
+    alignas(32) uint64_t buffer[batch_size];
+    std::fill(std::begin(initial), std::end(initial), bits);
+
+    auto r = bits;
+    auto n = 0.0;
+    auto e = std::complex<double>{1.0};
+
+    for (auto const& symmetry : batched_symmetries) {
+        std::copy(std::begin(initial), std::end(initial), std::begin(buffer));
+        symmetry.network(buffer);
+        for (auto i = 0U; i < batch_size; ++i) {
+            if (buffer[i] < r) {
+                r = buffer[i];
+                e = symmetry.eigenvalues[i];
+            }
+            else if (buffer[i] == bits) {
+                n += symmetry.eigenvalues[i].real();
+            }
+        }
+    }
+    for (auto const& symmetry : symmetries) {
+        auto const y = symmetry.network(bits);
+        if (y < r) {
+            r = y;
+            e = symmetry.eigenvalue;
+        }
+        else if (y == bits) {
+            n += symmetry.eigenvalue.real();
+        }
+    }
+
+    // We need to detect the case when norm is not zero, but only because of
+    // inaccurate arithmetics
+    constexpr auto norm_threshold = 1.0e-5;
+    if (std::abs(n) <= norm_threshold) { n = 0.0; }
+    LATTICE_SYMMETRIES_ASSERT(n >= 0.0, "");
+    auto const group_size = batch_size * batched_symmetries.size() + symmetries.size();
+    n                     = std::sqrt(n / static_cast<double>(group_size));
+
+    // Save results
+    representative = r;
+    character      = e;
+    norm           = n;
+}
+
+auto get_state_info(std::vector<big_symmetry_t> const& symmetries, bits512 const& bits,
+                    bits512& representative, std::complex<double>& character, double& norm) noexcept
+    -> void
+{
+    bits512 buffer;
+    auto    r = bits;
+    auto    n = 0.0;
+    auto    e = std::complex<double>{1.0};
+
+    for (auto const& symmetry : symmetries) {
+        buffer = bits;
+        symmetry.network(buffer);
+        if (buffer < r) {
+            r = buffer;
+            e = symmetry.eigenvalue;
+        }
+        else if (buffer == bits) {
+            n += symmetry.eigenvalue.real();
+        }
+    }
+
+    // We need to detect the case when norm is not zero, but only because of
+    // inaccurate arithmetics
+    constexpr auto norm_threshold = 1.0e-5;
+    if (std::abs(n) <= norm_threshold) { n = 0.0; }
+    LATTICE_SYMMETRIES_ASSERT(n >= 0.0, "");
+    n = std::sqrt(n / static_cast<double>(symmetries.size()));
+
+    // Save results
+    representative = r;
+    character      = e;
+    norm           = n;
+}
+
+} // namespace lattice_symmetries
+
+extern "C" ls_error_code ls_create_symmetry(ls_symmetry** ptr, unsigned const length,
+                                            unsigned const permutation[], bool const flip,
+                                            unsigned const sector)
+{
+    using namespace lattice_symmetries;
+    if (ptr == nullptr) { return LS_INVALID_ARGUMENT; }
+    auto const r = compile(tcb::span{permutation, length});
+    if (!r) {
+        if (r.error().category() == get_error_category()) {
+            return static_cast<ls_error_code>(r.error().value());
+        }
+        return LS_SYSTEM_ERROR;
+    }
+    auto periodicity = compute_periodicity(tcb::span{permutation, length});
+    if (flip && periodicity % 2U != 0U) { periodicity *= 2U; }
+    if (sector >= periodicity) { return LS_INVALID_SECTOR; }
+    auto const eigenvalue = compute_eigenvalue(sector, periodicity);
+
+    if (length > 64U) {
+        auto p = std::make_unique<ls_symmetry>(std::in_place_type_t<big_symmetry_t>{}, r.value(),
+                                               flip, sector, periodicity, eigenvalue);
+        *ptr   = p.release();
+    }
+    else {
+        auto p = std::make_unique<ls_symmetry>(std::in_place_type_t<small_symmetry_t>{}, r.value(),
+                                               flip, sector, periodicity, eigenvalue);
+        *ptr   = p.release();
+    }
+    return LS_SUCCESS;
+}
+
+extern "C" void ls_destroy_symmetry(ls_symmetry* symmetry)
+{
+    LATTICE_SYMMETRIES_CHECK(symmetry != nullptr, "trying to destroy a nullptr");
+    std::default_delete<ls_symmetry>{}(symmetry);
+}
+
+extern "C" unsigned ls_get_sector(ls_symmetry const* symmetry)
+{
+    LATTICE_SYMMETRIES_CHECK(symmetry != nullptr, "trying to dereference a nullptr");
+    return std::visit([](auto const& x) noexcept { return x.sector; }, symmetry->payload);
+}
+
+extern "C" bool ls_get_flip(ls_symmetry const* symmetry)
+{
+    LATTICE_SYMMETRIES_CHECK(symmetry != nullptr, "trying to dereference a nullptr");
+    return std::visit([](auto const& x) noexcept { return x.network.flip; }, symmetry->payload);
+}
+
+extern "C" unsigned ls_get_periodicity(ls_symmetry const* symmetry)
+{
+    LATTICE_SYMMETRIES_CHECK(symmetry != nullptr, "trying to dereference a nullptr");
+    return std::visit([](auto const& x) noexcept { return x.periodicity; }, symmetry->payload);
+}
+
+extern "C" void ls_get_eigenvalue(ls_symmetry const* symmetry, void* out)
+{
+    LATTICE_SYMMETRIES_CHECK(symmetry != nullptr, "trying to dereference a nullptr");
+    *reinterpret_cast<std::complex<double>*>(out) =
+        std::visit([](auto const& x) noexcept { return x.eigenvalue; }, symmetry->payload);
+}
+
+extern "C" double ls_get_phase(ls_symmetry const* symmetry)
+{
+    LATTICE_SYMMETRIES_CHECK(symmetry != nullptr, "trying to dereference a nullptr");
+    return std::visit(
+        [](auto const& x) noexcept {
+            return static_cast<double>(x.sector) / static_cast<double>(x.periodicity);
+        },
+        symmetry->payload);
+}
---
+++ src/symmetry.hpp
@@ -0,0 +1,99 @@
+// Copyright (c) 2019-2020, Tom Westerhout
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include "lattice_symmetries/lattice_symmetries.h"
+#include "network.hpp"
+#include <array>
+#include <complex>
+#include <variant>
+
+namespace lattice_symmetries {
+
+struct small_symmetry_t {
+    small_network_t      network;
+    unsigned             sector;
+    unsigned             periodicity;
+    std::complex<double> eigenvalue;
+
+    small_symmetry_t(fat_benes_network_t const& _network, bool _flip, unsigned _sector,
+                     unsigned _periodicity, std::complex<double> _eigenvalue) noexcept
+        : network{_network, _flip}
+        , sector{_sector}
+        , periodicity{_periodicity}
+        , eigenvalue{_eigenvalue}
+    {}
+};
+
+struct big_symmetry_t {
+    big_network_t        network;
+    unsigned             sector;
+    unsigned             periodicity;
+    std::complex<double> eigenvalue;
+
+    big_symmetry_t(fat_benes_network_t const& _network, bool _flip, unsigned _sector,
+                   unsigned _periodicity, std::complex<double> _eigenvalue) noexcept
+        : network{_network, _flip}
+        , sector{_sector}
+        , periodicity{_periodicity}
+        , eigenvalue{_eigenvalue}
+    {}
+};
+
+struct batched_small_symmetry_t {
+    static constexpr auto batch_size = batched_small_network_t::batch_size;
+
+    batched_small_network_t                      network;
+    std::array<unsigned, batch_size>             sectors;
+    std::array<unsigned, batch_size>             periodicities;
+    std::array<std::complex<double>, batch_size> eigenvalues;
+};
+
+template <class Int> auto compute_periodicity(tcb::span<Int const> permutation) -> unsigned;
+auto compute_eigenvalue(unsigned sector, unsigned periodicity) noexcept -> std::complex<double>;
+
+auto get_state_info(tcb::span<batched_small_symmetry_t const> batched_symmetries,
+                    tcb::span<small_symmetry_t const> symmetries, uint64_t bits,
+                    uint64_t& representative, std::complex<double>& character,
+                    double& norm) noexcept -> void;
+
+auto get_state_info(std::vector<big_symmetry_t> const& symmetries, bits512 const& bits,
+                    bits512& representative, std::complex<double>& character, double& norm) noexcept
+    -> void;
+
+} // namespace lattice_symmetries
+
+struct ls_symmetry {
+    std::variant<lattice_symmetries::small_symmetry_t, lattice_symmetries::big_symmetry_t> payload;
+
+    template <class T, class... Args>
+    ls_symmetry(std::in_place_type_t<T> tag, Args&&... args) noexcept
+        : payload{tag, std::forward<Args>(args)...}
+    {}
+};
