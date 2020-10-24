458dbae0611ee49639781d2931d9bf8bce891401
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-10-24 22:37:26 +0200

    Add ls_index test

--- test/src/test_api.cpp
+++ test/src/test_api.cpp
@@ -154,3 +154,91 @@ TEST_CASE("constructs basis", "[api]")
         ls_destroy_spin_basis(basis);
     }
 }
+
+TEST_CASE("finds correct states", "[api]")
+{
+    {
+        ls_error_code status;
+        // clang-format off
+        unsigned const sites[] =
+            { 0,  1,  2,  3,  4,  5,
+              6,  7,  8,  9, 10, 11,
+             12, 13, 14, 15, 16, 17,
+             18, 19, 20, 21, 22, 23};
+        unsigned const T_x[] =
+            { 1,  2,  3,  4,  5,  0,
+              7,  8,  9, 10, 11,  6,
+             13, 14, 15, 16, 17, 12,
+             19, 20, 21, 22, 23, 18};
+        unsigned const T_y[] =
+            { 6,  7,  8,  9, 10, 11,
+             12, 13, 14, 15, 16, 17,
+             18, 19, 20, 21, 22, 23,
+              0,  1,  2,  3,  4,  5};
+        unsigned const P_x[] =
+            {  5,  4,  3,  2,  1,  0,
+              11, 10,  9,  8,  7,  6,
+              17, 16, 15, 14, 13, 12,
+              23, 22, 21, 20, 19, 18};
+        unsigned const P_y[] =
+            {18, 19, 20, 21, 22, 23,
+             12, 13, 14, 15, 16, 17,
+              6,  7,  8,  9, 10, 11,
+              0,  1,  2,  3,  4,  5};
+        // clang-format on
+        ls_symmetry* T_x_symmetry = nullptr;
+        status = ls_create_symmetry(&T_x_symmetry, std::size(T_x), T_x, false, 0);
+        REQUIRE(status == LS_SUCCESS);
+
+        ls_symmetry* T_y_symmetry = nullptr;
+        status = ls_create_symmetry(&T_y_symmetry, std::size(T_y), T_y, false, 0);
+        REQUIRE(status == LS_SUCCESS);
+
+        ls_symmetry* P_x_symmetry = nullptr;
+        status = ls_create_symmetry(&P_x_symmetry, std::size(P_x), P_x, false, 0);
+        REQUIRE(status == LS_SUCCESS);
+
+        ls_symmetry* P_y_symmetry = nullptr;
+        status = ls_create_symmetry(&P_y_symmetry, std::size(P_y), P_y, false, 0);
+        REQUIRE(status == LS_SUCCESS);
+
+        ls_symmetry const* generators[] = {T_x_symmetry, T_y_symmetry, P_x_symmetry, P_y_symmetry};
+        ls_group*          group        = nullptr;
+        status = ls_create_group(&group, std::size(generators), generators);
+        REQUIRE(status == LS_SUCCESS);
+        ls_destroy_symmetry(T_x_symmetry);
+        ls_destroy_symmetry(T_y_symmetry);
+        ls_destroy_symmetry(P_x_symmetry);
+        ls_destroy_symmetry(P_y_symmetry);
+
+        ls_spin_basis* basis = nullptr;
+        status               = ls_create_spin_basis(&basis, group, 24, 12);
+        REQUIRE(status == LS_SUCCESS);
+
+        ls_destroy_group(group);
+
+        status = ls_build(basis);
+        REQUIRE(status == LS_SUCCESS);
+
+        uint64_t count;
+        status = ls_get_number_states(basis, &count);
+        REQUIRE(status == LS_SUCCESS);
+        REQUIRE(count == 29606U);
+
+        ls_states* states = nullptr;
+        status            = ls_get_states(&states, basis);
+        REQUIRE(status == LS_SUCCESS);
+
+        auto const begin = ls_states_get_data(states);
+        auto const end   = begin + count;
+        for (auto i = 0UL; i < count; ++i) {
+            uint64_t index;
+            status = ls_get_index(basis, &begin[i], &index);
+            REQUIRE(status == LS_SUCCESS);
+            REQUIRE(index == i);
+        }
+
+        ls_destroy_states(states);
+        ls_destroy_spin_basis(basis);
+    }
+}
