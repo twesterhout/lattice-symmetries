c7d46e02610c0dc96c37d0922bff48b57fadff55
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-11-26 00:53:05 +0100

    Version bump since we changed the interface

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lattice-symmetries",
-    version="0.1.1",
+    version="0.2.0",
     description="Easily work with quantum many-body bases taking all lattice symmetries into account",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
