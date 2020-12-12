b45473d37a918049b87a05707599d0233e0da85f
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-12-12 16:51:10 +0100

    Testing cast_to_basis

---
+++ python/test/test_basis.py
@@ -0,0 +1,34 @@
+import numpy as np
+import lattice_symmetries as ls
+
+
+def test_4_spins():
+    # fmt: off
+    matrix = np.array([[1,  0,  0, 0],
+                       [0, -1,  2, 0],
+                       [0,  2, -1, 0],
+                       [0,  0,  0, 1]])
+    # fmt: on
+    number_spins = 4
+    edges = [(i, (i + 1) % number_spins) for i in range(number_spins)]
+
+    basis = ls.SpinBasis(ls.Group([]), number_spins=4, hamming_weight=2)
+    basis.build()
+    assert basis.number_states == 6
+    operator = ls.Operator(basis, [ls.Interaction(matrix, edges)])
+    assert np.isclose(ls.diagonalize(operator, k=1)[0], -8)
+
+    basis = ls.SpinBasis(ls.Group([]), number_spins=4, hamming_weight=2, spin_inversion=1)
+    basis.build()
+    assert basis.number_states == 3
+    operator = ls.Operator(basis, [ls.Interaction(matrix, edges)])
+    assert np.isclose(ls.diagonalize(operator, k=1)[0], -8)
+
+    T = ls.Symmetry([1, 2, 3, 0], sector=0)
+    basis = ls.SpinBasis(ls.Group([T]), number_spins=4, hamming_weight=2, spin_inversion=1)
+    basis.build()
+    assert basis.number_states == 2
+    operator = ls.Operator(basis, [ls.Interaction(matrix, edges)])
+    assert np.isclose(ls.diagonalize(operator, k=1)[0], -8)
+
+test_4_spins()
