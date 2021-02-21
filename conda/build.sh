5def5a59ad699f068308a8101832966d1272039a
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-21 10:15:44 +0100

    Formatting + fix globbing issue reported by Codacy

--- conda/build.sh
+++ conda/build.sh
@@ -4,22 +4,22 @@ set -ex
 
 mkdir -p build.shared
 pushd build.shared
-rm -rf *
+rm -rf -- *
 cmake -GNinja \
-      -DCMAKE_BUILD_TYPE=Release \
-	  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-	  -DBUILD_SHARED_LIBS=ON \
-	  -DLatticeSymmetries_ENABLE_UNIT_TESTING=OFF \
-	  -DLatticeSymmetries_ENABLE_CLANG_TIDY=OFF \
-	  -DLatticeSymmetries_ENABLE_CPPCHECK=OFF \
-	  -DLatticeSymmetries_ENABLE_CODE_COVERAGE=OFF \
-	  -DLatticeSymmetries_LINK_STDLIB_STATICALLY=OFF \
-	  ..
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DBUILD_SHARED_LIBS=ON \
+  -DLatticeSymmetries_ENABLE_UNIT_TESTING=OFF \
+  -DLatticeSymmetries_ENABLE_CLANG_TIDY=OFF \
+  -DLatticeSymmetries_ENABLE_CPPCHECK=OFF \
+  -DLatticeSymmetries_ENABLE_CODE_COVERAGE=OFF \
+  -DLatticeSymmetries_LINK_STDLIB_STATICALLY=OFF \
+  ..
 cmake --build .
 find . -name "*.so*" -maxdepth 1 -type f | while read sofile; do
-    echo "Setting rpath of $sofile to" '$ORIGIN'
-    patchelf --set-rpath '$ORIGIN' --force-rpath $sofile
-    patchelf --print-rpath $sofile
+  echo "Setting rpath of $sofile to" '$ORIGIN'
+  patchelf --set-rpath '$ORIGIN' --force-rpath $sofile
+  patchelf --print-rpath $sofile
 done
 cmake --build . --target install
 popd
