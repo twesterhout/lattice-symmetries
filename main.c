747926fbff50badf0e4a2d4c3cf6bc7091424c5c
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2022-03-23 15:12:58 +0100

    Let's try using -dynamiclib

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 #include <stdio.h>
 
 int main() {
-  ls_enable_logging();
-  ls_hs_init();
-  ls_hs_spin_basis_v1 basis;
-  ls_hs_operator_v1 operator;
-  ls_hs_basis_and_hamiltonian_from_yaml("heisenberg_chain_4.yaml", &basis,
-                                        &operator);
-  printf("number_spins = %u\n", ls_get_number_spins(basis.payload));
-  ls_hs_destroy_operator(&operator);
-  ls_hs_destroy_spin_basis(&basis);
-  ls_hs_exit();
+  ls_hs_combinadics_kernel_data *x =
+      ls_hs_internal_create_combinadics_kernel_data(64, false);
+  return 0;
 }
