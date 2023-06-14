fe66be1dcec3fcbbf02091702164395f14b4fa5c
twesterhout <14264576+twesterhout@users.noreply.github.com>
2023-06-14 16:21:20 +0200

    Removing old files

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "lattice_symmetries_haskell.h"
-#include <stdio.h>
-
-int main() {
-  ls_hs_combinadics_kernel_data *x =
-      ls_hs_internal_create_combinadics_kernel_data(64, false);
-  return 0;
-}
