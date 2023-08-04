8ad71d01aaa968a5df9d977cfa4913e75d8c93ce
twesterhout <14264576+twesterhout@users.noreply.github.com>
2023-08-04 14:59:23 +0200

    Template test

--- flake.nix
+++ flake.nix
@@ -126,7 +126,7 @@
       overlays.default = composed-overlay { withPic = true; };
 
       templates.default = {
-        path = builtins.toPath ./template;
+        path = builtins.toPath "${./.}/template";
         description = "Python project template that uses lattice-symmetries";
       };
 
